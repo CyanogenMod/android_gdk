9eb23e0856e35a4649a3678d13ddaa7b585251e8
Joseph Wen <josephwen@google.com>
2011-06-29 17:50:33 -0700

    Add samples bitmap-plasma-llvm and hello-llvm
    Change-Id: Ib98fbd49079054b8c7e1caa343c177c34d7800e1

---
+++ samples/bitmap-plasma-llvm/jni/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := -I/usr/local/google/work/m/frameworks/compile/libbcc/include/
+
+LOCAL_LDFLAGS := -L/usr/local/google/work/m/out/target/product/crespo4g/system/lib/
+
+LOCAL_MODULE    := plasma
+LOCAL_SRC_FILES := plasmaLLVM.c
+LOCAL_LDLIBS    := -lm -llog -ljnigraphics -lbcc
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+ROOT := /usr/local/google/work/m/
+
+LOCAL_MODULE    := plasma_portable
+
+LOCAL_CFLAGS := -I $(ROOT)/frameworks/compile/libbcc/include/ \
+  -I $(ROOT)/external/clang/lib/Headers/ \
+  -I ../../platforms/android-100/arch-arm/usr/include/ 
+LOCAL_SRC_FILES := libplasma.c
+
+include $(BUILD_BITCODE)
---
+++ samples/hello-llvm/jni/test.c
@@ -0,0 +1,7 @@
+#ifndef NUM
+#define NUM 5566
+#endif
+
+int test_func2() {
+  return NUM;
+}
