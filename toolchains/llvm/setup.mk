e929a698abf4470542eb418740f537869447f1bd
Nowar Gu <nowar100@gmail.com>
2011-07-04 14:26:09 +0800

    Fix cmd-build-bitcode defined commands.
    I found that this architecture is not good.
    For example, jni/Application.mk cannot use ARM and LLVM at the
    same time. This bug is because of the rules in build/core/.
    
    I will do a big refactoring on <GDK>/build/core and de-coupling
    this from <NDK>/build/core these days.

--- toolchains/llvm/setup.mk
+++ toolchains/llvm/setup.mk
@@ -1,33 +1,33 @@
 TOOLCHAIN_PREFIX := $(TOOLCHAIN_PREFIX:%-=%)
 
-TARGET_CC       := $(TOOLCHAIN_PREFIX)llvm-ndk-cc
-TARGET_CFLAGS   :=
-
-TARGET_CXX      := $(TOOLCHAIN_PREFIX)llvm-ndk-cc
-TARGET_CXXFLAGS := $(TARGET_CFLAGS) -fno-exceptions -fno-rtti -D __cplusplus
-
-TARGET_LD       := $(TOOLCHAIN_PREFIX)llvm-ndk-link
-TARGET_LDFLAGS  :=
-
-TARGET_AR       := ar
-TARGET_ARFLAGS  := crs
-
-TARGET_C_INCLUDES := \
-    $(SYSROOT)/usr/include
+#TARGET_CC       := $(TOOLCHAIN_PREFIX)llvm-ndk-cc
+#TARGET_CFLAGS   :=
+#
+#TARGET_CXX      := $(TOOLCHAIN_PREFIX)llvm-ndk-cc
+#TARGET_CXXFLAGS := $(TARGET_CFLAGS) -fno-exceptions -fno-rtti -D __cplusplus
+#
+#TARGET_LD       := $(TOOLCHAIN_PREFIX)llvm-ndk-link
+#TARGET_LDFLAGS  :=
+#
+#TARGET_AR       := ar
+#TARGET_ARFLAGS  := crs
+#
+#TARGET_C_INCLUDES := \
+#    $(SYSROOT)/usr/include
 
 #
 # Break build/core/default-build-commands.mk rules
 #
-TARGET_NO_UNDEFINED_LDFLAGS := 
-TARGET_NO_EXECUTE_CFLAGS  := 
-TARGET_NO_EXECUTE_LDFLAGS :=
-TARGET_LDLIBS := 
-cmd-strip := 
+#TARGET_NO_UNDEFINED_LDFLAGS := 
+#TARGET_NO_EXECUTE_CFLAGS  := 
+#TARGET_NO_EXECUTE_LDFLAGS :=
+#TARGET_LDLIBS := 
+#cmd-strip := 
 
 define cmd-build-bitcode
-$(TARGET_LD) \
-		$(call host-path, $(PRIVATE_OBJECTS)) \
-		$(PRIVATE_LDFLAGS) \
-		$(PRIVATE_LDLIBS) \
-		-o $(call host-path, $@)
+$(BITCODE_LD) \
+    $(call host-path, $(PRIVATE_OBJECTS)) \
+    $(PRIVATE_LDFLAGS) \
+    $(PRIVATE_LDLIBS) \
+    -o $(call host-path, $@)
 endef
