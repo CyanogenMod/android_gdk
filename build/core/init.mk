53107313f28dd11e7edb9543526796b0461959ba
Nowar Gu <nowar100@gmail.com>
2011-07-05 15:43:52 +0800

    Add clear-vars.mk.

--- build/core/init.mk
+++ build/core/init.mk
@@ -5,4 +5,5 @@ include $(NDK_ROOT)/build/core/definitions.mk
 GDK_PLATFORM_ROOT := $(GDK_ROOT)/platforms/android-portable/arch-llvm
 GDK_TOOLCHAIN_ROOT := $(GDK_ROOT)/toolchains/llvm
 
+CLEAR_VARS := $(GDK_ROOT)/build/core/clear-vars.mk
 BUILD_BITCODE := $(GDK_ROOT)/build/core/build-bitcode.mk
