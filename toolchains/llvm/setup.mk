ec14e1f3eee88971aa4321b1ea9a48863dec1875
Nowar Gu <nowar100@gmail.com>
2011-07-05 15:44:30 +0800

    Fix missing changes.

--- toolchains/llvm/setup.mk
+++ toolchains/llvm/setup.mk
@@ -1,13 +1,13 @@
 GDK_TOOLCHAIN_PREFIX := $(GDK_TOOLCHAIN_ROOT)/llvm/prebuilt/
 
 # TODO(Nowar): In future, we use llvm-ndk-cc.
-BITCODE_CC       := $(TOOLCHAIN_PREFIX)clang
+BITCODE_CC       := $(GDK_TOOLCHAIN_PREFIX)clang
 BITCODE_CFLAGS   := -ccc-host-triple armv7-none-linux-gnueabi -emit-llvm
 
-BITCODE_CPP      := $(TOOLCHAIN_PREFIX)clang++
+BITCODE_CPP      := $(GDK_TOOLCHAIN_PREFIX)clang++
 BITCODE_CPPFLAGS := $(BITCODE_CFLAGS) -fno-exceptions -fno-rtti -D __cplusplus
 
-BITCODE_LD       := $(TOOLCHAIN_PREFIX)llvm-link
+BITCODE_LD       := $(GDK_TOOLCHAIN_PREFIX)llvm-link
 BITCODE_LDFLAGS  :=
 
 BITCODE_C_INCLUDES := \
