dd67157c454dea3378dc0a92c3d389a4a853731d
Flavio Lerda <flerda@google.com>
2011-07-06 08:48:01 +0100

    Fix broken build.
    Remove Android.mk files for GDK samples.
    
    Change-Id: I9e40b97777520cba73d660546cd27ca707fffd5d

--- samples/hello-llvm/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := hellollvmDSO
-LOCAL_CFLAGS    := -D NUM=7788
-LOCAL_SRC_FILES := hello_llvm.c test.cpp
-
-include $(BUILD_SHARED_LIBRARY)
